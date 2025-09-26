223cff493bb0b4cc3fef66c022047677f9c833d8
Nico Kempe <git@nicokempe.de>
2025-09-26 20:15:32 +0200

    feat(app): add nuxt pages

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,24 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLoadingIndicator
+      color="repeating-linear-gradient(to right,#10b981 0%,#047857 50%,#064e3b 100%)"
+    />
+    <NuxtLayout>
+      <NuxtPage
+        :transition="{ name: 'page', mode: 'out-in' }"
+      />
+    </NuxtLayout>
   </div>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
