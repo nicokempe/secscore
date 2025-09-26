84c8824ef289b7d62a84da25464a2d73d8443192
Nico Kempe <git@nicokempe.de>
2025-09-26 19:23:54 +0200

    feat(nuxt): extend config with installed modules and custom configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,46 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: '2025-09-26',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils']
+  modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxtjs/turnstile', '@nuxtjs/tailwindcss'],
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+  nitro: {
+    preset: 'cloudflare_module',
+    experimental: {
+      openAPI: true,
+    },
+  },
+  tailwindcss: {
+    config: {
+      content: ['./app/pages/**/*.{vue,js,ts}', './app/components/**/*.{vue,js,ts}', './app/layouts/**/*.{vue,js,ts}'],
+    },
+  },
+  turnstile: {
+    siteKey: process.env.CLOUDFLARE_TURNSTILE_SITE_KEY,
+  },
+  css: [
+    '~/assets/css/tailwind.css',
+  ],
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      titleTemplate: '%s • SecScore',
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width,initial-scale=1,shrink-to-fit=no' },
+        { id: 'og:site_name', property: 'og:site_name', content: 'SecScore' },
+        { id: 'twitter:card', name: 'twitter:card', content: 'summary' },
+        { id: 'twitter:site', name: 'twitter:site', content: '@nico_kempe' },
+        { id: 'twitter:creator', name: 'twitter:creator', content: '@nico_kempe' },
+        { id: 'twitter:creator-id', name: 'twitter:creator:id', content: '990948799504842754' },
+        { id: 'twitter:site', name: 'twitter:site', content: '@nico_kempe' },
+        { id: 'twitter:site-id', name: 'twitter:site:id', content: '990948799504842754' },
+      ],
+    },
+  },
 })
