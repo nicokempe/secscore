1b0af3eedd0ba4fab66e3c1f936768b44618c655
Nico Kempe <git@nicokempe.de>
2025-09-26 19:36:14 +0200

    feat(dx): add eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,20 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt(
-  // Your custom configs here
-)
+  {
+    rules: {
+      // Errors
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/semi': ['error', 'always'],
+      'brace-style': ['error', 'stroustrup'],
+      // Warnings
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-this-alias': 'warn',
+      'vue/no-unused-vars': 'warn',
+      'no-useless-escape': 'warn',
+      // Off
+      'vue/no-deprecated-filter': 'off',
+    },
+  },
+);
